39b0596d63130b3f9c5fff154d8e52ccf2844085
Akhilesh Yadav <aksyvakin@gmail.com>
2022-07-04 07:47:03 +0000

    Update next to 11 verison

--- next.config.js
+++ next.config.js
@@ -3,15 +3,21 @@ let basePath="";
 if(process.env.NODE_ENV!=='development') {
   basePath="/dist";
 }
-module.exports = withImages({
-  fileExtensions: ["jpg", "jpeg", "png", "gif"],
-  esModule: true,
-  basePath,
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(gif|svg|jpg|png)$/,
-      loader: "file-loader",
-    })
-    return config
-  }
-})
+
+module.exports = {
+  swcMinify: true,
+}
+
+// module.exports = withImages({
+//   fileExtensions: ["jpg", "jpeg", "png", "gif"],
+//   esModule: true,
+//   basePath,
+  
+//   webpack(config, options) {
+//     config.module.rules.push({
+//       test: /\.(gif|svg|jpg|png)$/,
+//       loader: "file-loader",
+//     })
+//     return config
+//   }
+// })
